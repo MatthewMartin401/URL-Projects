5e14766c36a17b928c6febadba18666afd198487
MatthewMartin401 <129974886+MatthewMartin401@users.noreply.github.com>
2025-11-05 18:31:20 +0000

    Created HmblBndl_BookSearch
    Completed code for HmblBndl_BookSearch.
    
    This project collects all items names and opens the items within a browser.
    
    This is achieved by opening in selenium, so that all elements are loaded, and parsing and storing the book titles. These titles are then opened again in the browser.

---
+++ HmblBndl_BookSearch.py
@@ -0,0 +1,40 @@
+#  Project: Project-HmblBndl_BookSearch.
+#  Creator: Me.
+#  Status: Complete.
+
+'''
+This project is suppost to collect a list of all of the Books on offer in an Bundle on Humble Bundle,
+and open a tab for each item. This is to be used for convience when looking into the contents of a bundle.
+'''
+
+import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import urllib.parse
+
+
+#  Goes to website and gets all items
+URL = ""
+if URL == "" or URL == None:
+    URL = input("Please enter Bundle URL: \n")
+browser = webdriver.Chrome()
+browser.get(URL)
+itemName = "item-title"  #  Not loading in the html. Needs a delay which requests library doesn't support. Changed to Selenium.
+itemNameList = []
+browser.fullscreen_window()  #  Elements change depending on window size.
+time.sleep(2)
+text = browser.find_elements(By.CLASS_NAME, itemName)  #  Gets all of the items displayed on the page.
+
+#  Converts to string and add items to list.
+for webElem in list(text):
+    itemNameList.append(str(webElem.text))
+browser.quit()
+
+#  Opens items within the browser.
+searchURL = "https://www.google.com/search"  #  Search Engine url link.
+for val in itemNameList:
+    param = {'q' : str(val)}
+    added_param = urllib.parse.urlencode(param)  #  Adds parameters onto URL
+    encoded_url = f'{searchURL}?{added_param}'  #  Final URL
+    webbrowser.open(url=encoded_url)
